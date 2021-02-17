47245003be1d52b4f8f1beca0149235326d9ee9c
andrewmelville <andrewmelville.16@gmail.com>
2021-02-17 18:02:35 +0000

    lstm train

--- LSTM_Simulated_Data.py
+++ LSTM_Simulated_Data.py
@@ -32,14 +32,17 @@ tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
 #%% Simulating Data
 
 
-# Define a generative model to simulate 10000 days of data for 1 currency basket and 30 commodities
-model = model_generator()
-sim_currency = model.linear_model(num_obs=10000, num_covariates=30, beta_type='brownian', noise=0.001)
-sim_betas = model.params
-sim_commods = model.covariates()['Noisy']
-model.model_plot()
+# # Define a generative model to simulate 10000 days of data for 1 currency basket and 30 commodities
+# model = model_generator()
+# sim_currency = model.linear_model(num_obs=10000, num_covariates=30, beta_type='brownian', noise=0.001)
+# sim_betas = model.params
+# sim_commods = model.covariates()['Noisy']
+# model.model_plot()
 
 next_day_returns = pd.DataFrame(sim_commods[1].iloc[1:])
+# CBOT_OATS_df = pd.read_csv('Data/Continuous Futures Series/CBOT Rough Rice.csv',
+#                            index_col = 0, skiprows = 0, skipfooter = 1, header = 1, engine = 'python')
+# next_day_returns = pd.DataFrame(CBOT_OATS_df['Close'].diff(1))
 #%% Train (70%) / Validate (20%) / Test (10%)
 def makeXy(series_df, nb_timesteps):
     """
@@ -53,38 +56,41 @@ def makeXy(series_df, nb_timesteps):
     # Split data into train/val/test sets
     n = len(series_df)
     
+    # Grab column name
+    col_name = series_df.columns[0]
+    
     # Split full data into train, validation, and test sets
-    train_unscaled = pd.DataFrame(series_df[0:int(n*0.7)]).reset_index()
-    val_unscaled = pd.DataFrame(series_df[int(n*0.7):int(n*0.9)]).reset_index()
-    test_unscaled = pd.DataFrame(series_df[int(n*0.9):]).reset_index()
+    train_unscaled = pd.DataFrame(series_df[0:int(n*0.7)]).reset_index(drop=True)
+    val_unscaled = pd.DataFrame(series_df[int(n*0.7):int(n*0.9)]).reset_index(drop=True)
+    test_unscaled = pd.DataFrame(series_df[int(n*0.9):]).reset_index(drop=True)
     
     # Standardise data with respect to the training data
     # (would expect data very close to a normal given the method of data simulation)
     # scaler = StandardScaler()
     scaler = MinMaxScaler()
-    scaler.fit(train_df)
+    scaler.fit(train_unscaled)
     
-    train_scaled = pd.DataFrame(scaler.transform(np.array(train_unscaled[1]).reshape(-1,1)))
-    val_scaled = pd.DataFrame(scaler.transform(np.array(val_unscaled[1]).reshape(-1,1)))
-    test_scaled = pd.DataFrame(scaler.transform(np.array(test_unscaled[1]).reshape(-1,1)))
+    train_scaled = pd.DataFrame(scaler.transform(train_unscaled[col_name].values.reshape(-1,1)))
+    val_scaled = pd.DataFrame(scaler.transform(val_unscaled[col_name].values.reshape(-1,1)))
+    test_scaled = pd.DataFrame(scaler.transform(test_unscaled[col_name].values.reshape(-1,1)))
     
     # Reshape data to be vectors of length nb_timesteps and labels
-    train_X, train_y = [], np.array([returns for returns in train_unscaled[1].loc[nb_timesteps:].shift(-1)])
-    val_X, val_y = [], np.array([returns for returns in val_unscaled[1].loc[nb_timesteps:].shift(-1)])
-    test_X, test_y = [], np.array([returns for returns in test_unscaled[1].loc[nb_timesteps:].shift(-1)])
+    train_X, train_y = [], np.array([returns for returns in train_unscaled[col_name].shift(-1)])[nb_timesteps:-1]
+    val_X, val_y = [], np.array([returns for returns in val_unscaled[col_name].shift(-1)])[nb_timesteps:-1]
+    test_X, test_y = [], np.array([returns for returns in test_unscaled[col_name].shift(-1)])[nb_timesteps:-1]
     
     # Train
-    for i in range(nb_timesteps, train_scaled[0].shape[0]):
+    for i in range(nb_timesteps, train_scaled[0].shape[0]-1):
         train_X.append(list(train_scaled[0].loc[i-nb_timesteps:i-1]))
     train_X = np.array(train_X)
     
     # Validation
-    for i in range(nb_timesteps, val_scaled[0].shape[0]):
+    for i in range(nb_timesteps, val_scaled[0].shape[0]-1):
         val_X.append(list(val_scaled[0].loc[i-nb_timesteps:i-1]))
     val_X = np.array(val_X)
     
     # Test
-    for i in range(nb_timesteps, test_scaled[0].shape[0]):
+    for i in range(nb_timesteps, test_scaled[0].shape[0]-1):
         test_X.append(list(test_scaled[0].loc[i-nb_timesteps:i-1]))
     test_X = np.array(test_X)
 
@@ -96,20 +102,13 @@ def makeXy(series_df, nb_timesteps):
     test_X =  tf.convert_to_tensor(test_X, dtype="float32")
     test_y =  tf.convert_to_tensor(test_y, dtype="float32")
     
-    return train_X, train_y, val_X, val_y, test_X, test_y
+    return train_X, train_y, val_X, val_y, test_X, test_y, scaler
 
 
         
 
-X_train, y_train, X_val, y_val, X_test, y_test = makeXy(next_day_returns, 600)
+X_train, y_train, X_val, y_val, X_test, y_test, scaler = makeXy(next_day_returns.dropna(), 600)
 print('Shape of train arrays:', X_train.shape, y_train.shape)
-
-# The input to RNN layers must be of shape (number of samples, number of timesteps, number of features per timestep).
-#  In this case we are modeling only air pressure, hence number of features per timestep is one. 
-# The number of timesteps is seven and number of samples is same as the number of samples in X_train and X_val, 
-# which are reshaped to 3D arrays.
-# X_train, X_val = X_train.reshape((X_train.shape[0], X_train.shape[1], 1)), X_val.reshape((X_val.shape[0], X_val.shape[1], 1))
-# print('Shape of 3D arrays:', X_train.shape, X_val.shape)
 #%%
 
 # Now we define the neural network topology using the Keras Functional API. In this approach a layer can be declared as the input of the following layer at the time of defining the next layer. 
@@ -132,13 +131,13 @@ lstm_layer = LSTM(10, input_shape=(600,1), return_sequences=False)(input_layer)
 dropout_layer = Dropout(0.2)(lstm_layer)
 
 # Finally, the output layer gives prediction for the next day's air pressure. This should be a real number value, and so no activation function is used (i.e. linear activation).
-dense_layer = Dense(20, activation='tanh')(dropout_layer)
-output_layer = Dense(1, activation='tanh')(dense_layer)
+# dense_layer = Dense(5, activation='tanh')(dropout_layer)
+output_layer = Dense(2, activation='tanh')(dropout_layer)
 
 #%% Defining custom loss function to be used in model training
 
 def profit_loss(y_true, y_pred):
-    profit = tf.math.multiply(y_true, 1/y_pred) 
+    profit = tf.math.multiply(y_true, -100*y_pred) 
     return profit
 
 keras.losses.profit_loss = profit_loss
@@ -151,21 +150,23 @@ ts_model = Model(inputs=input_layer, outputs=output_layer)
 ts_model.compile(loss=loss_profit, optimizer=opt)
 ts_model.summary()
 
-save_weights_at = os.path.join('keras_models', 'Sim_Data_LSTM_weights.{epoch:02d}-{val_loss:.4f}.hdf5')
+save_weights_at = os.path.join('keras_models', 'Sim_Data_LSTM_weights')
+# save_weights_at = os.path.join('keras_models', 'Sim_Data_LSTM_weights.{epoch:02d}-{val_loss:.4f}.hdf5')
 # save_weights_at = os.path.join('keras_models', 'PRSA_data_Air_Pressure_LSTM_weights.01.hdf5')
 
 save_best = ModelCheckpoint(save_weights_at, monitor='val_loss', verbose=0,
                             save_best_only=True, save_weights_only=False, mode='min',
                             period=1)
-ts_model.fit(x=X_train, y=y_train, batch_size=16, epochs=4,
+ts_model.fit(x=X_train, y=y_train, batch_size=32, epochs=1,
              verbose=1, callbacks=[save_best], validation_data=(X_val, y_val),
              shuffle=True)
 
 
 # Prediction are made for the air pressure from the best saved model. The model's predictions, which are on the scaled  air-pressure, are inverse transformed to get predictions on original air pressure. The goodness-of-fit, R-squared is also calculated for the predictions on the original variable.
 # In[ ]:
-
-best_model = load_model(os.path.join('keras_models', 'Sim_Data_LSTM_weights.01--0.0203.hdf5'), custom_objects={'profit_loss':profit_loss})
+    
+best_model = load_model(os.path.join('keras_models', 'Sim_Data_LSTM_weights'), custom_objects={'profit_loss':profit_loss})
+# best_model = load_model(os.path.join('keras_models', 'Sim_Data_LSTM_weights.01-0.9380.hdf5'), custom_objects={'profit_loss':profit_loss})
 # best_model = load_model(os.path.join('keras_models', 'PRSA_data_Air_Pressure_LSTM_weights.02-0.0128.hdf5'))
 preds = best_model.predict(X_val)
 pred_PRES = scaler.inverse_transform(preds)
@@ -175,7 +176,7 @@ pred_PRES = np.squeeze(pred_PRES)
 
 from sklearn.metrics import r2_score
 r2 = r2_score(y_val, pred_PRES)
-print('R-squared on validation set of the original air pressure:', r2)
+print('R-squared on validation set of the returns:', r2)
 
 
 # Let's plot the first 50 actual and predicted values of air pressure.
@@ -184,6 +185,7 @@ print('R-squared on validation set of the original air pressure:', r2)
 import matplotlib.pyplot as plt    
 
 signal_plot(np.array(y_val), pred_PRES)
+plt.hist(pred_PRES, bins=100)
 # def pred_plot(actual_y, pred_y):
 
 #     plt.figure(figsize=(10, 10))
